5d7f5b86b84fef2a1a682d355399d69509b5550c
Andrew <blazersand2000@yahoo.com>
2020-11-09 21:11:58 -0500

    Convert to .NET Core

--- ImgConsole/Program.cs
+++ ImgConsole/Program.cs
@@ -12,20 +12,33 @@ namespace ImgConsole
 
       Bitmap bmp = Bitmap.FromFile(inFile);
 
+      int midX = bmp.Pixels.NumColumns / 2;
+      int midY = bmp.Pixels.NumRows / 2;
+
       for (int row = 0; row < bmp.Pixels.NumRows; row++)
       {
         for (int col = 0; col < bmp.Pixels.NumColumns; col++)
         {
           Pixel px = bmp.Pixels[row, col];
 
+          int distFromMidX = Math.Abs(midX - col);
+          int distFromMidY = Math.Abs(midY - row);
+          int distance = (int)Math.Sqrt(Math.Pow(distFromMidX, 2) + Math.Pow(distFromMidY, 2));
+
           //Simple pixel manipulation example
-          px.Red = (byte)Math.Min(255, px.Red + 50);
-          px.Green = (byte)Math.Min(255, px.Green + 50);
-          px.Blue = (byte)Math.Min(255, px.Blue + 50);
+          byte value = (byte)RestrictRange(0, 255, (int)(((float)distance / Math.Max(midY, 1)) * 255) - 100);
+          px.Red = value;
+          px.Green = value;
+          px.Blue = value;
         }
       }
 
       bmp.SaveFile(outFile);
+    }
+
+    private static int RestrictRange(int min, int max, int input)
+    {
+      return Math.Min(Math.Max(input, min), max);
     }
   }
 }
